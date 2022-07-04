816b3b0c3ceecfb0ebb2e55c6dfc96e6074035d8
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-04 12:44:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 3;//
+            int[] array = new int[3];
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            // parameterarray(array);
+            parameterarray(1, 2, 3,y);
+
+
+        }
+        public static void parameterarray(params int[] array)
+        {
+            Console.WriteLine("Elments Are {0}", array.Length);
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+                    
+            }
+        }
+    }
+}
